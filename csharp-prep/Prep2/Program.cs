9fd919ca28732e0526aecf9b67b1a1a83f84b440
Francogallardo99 <gal24016@byui.edu>
2024-11-08 19:11:09 -0300

    Prep 2 ready

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What was your grade percentage? ");
+        string gradePercentage= Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+        string letter = "";
+
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine ($"You have an {letter} in your exam");
+
+        if (grade >=70)
+        {
+            Console.WriteLine("Congratulations! You pass the exam");
+        }
+        else
+        {
+            Console.WriteLine("You can try the next time");
+        }
+    } 
 }
