059a1679fd2bfbb0da73b8a907d749fd2fde7f84
Francogallardo99 <gal24016@byui.edu>
2024-11-09 00:13:06 -0300

    Part

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        while ( number == magicNumber)
+        {
+            Console.Write("What is the magic number? ");
+            string magic = Console.ReadLine();
+            int magicNumber = int.Parse(magic);
+            Console.Write("Give me a number: ");        
+            string numberTry = Console.ReadLine();
+            int number = int.Parse(numberTry);
+
+        
+            
+        }
     }
 }
